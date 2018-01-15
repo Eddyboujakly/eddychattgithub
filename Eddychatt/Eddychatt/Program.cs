457bc6385bf64831a66471d3720e482bffd6035f
Edward Boujakly <edward.boujakly@learnet.se>
2018-01-15 12:22:14 +0100

    klar

---
+++ Eddychatt/Eddychatt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eddychatt
+{
+    //Av Edward boujakly 2018-01-15
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
